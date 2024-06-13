d42de06871831298b5dabe2ae8966d9bdf4d7a70
MaMR1 <90766008+MaMR1@users.noreply.github.com>
2024-06-13 14:45:04 +0200

    Update and rename 1-cartes.py to cartes.py

--- App/src/cartes.py
+++ App/src/cartes.py
@@ -1,9 +1,11 @@
+# Bibliothèques standard
 import random
 
-#Le fichier avec toutes les cartes à initier
-#Dépend de la classe Carte : Couleur / Numéro
+# Modules Personnels
+...
 
-#La classe pour créer une carte avec les attributs de couleur et de numéro
+
+# La classe pour créer une carte avec les attributs de couleur et de numéro
 class Carte:
     def __init__(self, color, number):
         self.color = color
@@ -12,29 +14,27 @@ class Carte:
     def __str__(self):
         return f"{self.number} \\ {self.color}"
 
-
 def generer_paquet():
     couleurs = ['Coeur', 'Carreau'] #, 'Trèfle', 'Pique'
     valeurs = ['2', '3', '4', '5']  #, '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As'
-    paquet = [Carte(couleur, valeur) for couleur in couleurs for valeur in valeurs]
+    paquet = [Carte(i, j) for i in couleurs for j in valeurs]
     return paquet
 
 def melanger_paquet(paquet):
     random.shuffle(paquet)
-    return paquet      # Convertir la liste en tuple 
+    return paquet           # Convertir la liste en tuple 
                             # Pour éviter qu'elle ne se mélange à nouveau à la distribution
 
 def afficher_paquet(paquet):
-    for carte in paquet:
-        print(carte)
-    print("FIN")
+    for i in paquet:
+        print(i, end= "\t")
 
 def afficher_carte_precise(paquet, index):
     print(f"{paquet[index]}")
 
 
-if __name__ == "__main__":
-
+# MAIN FUNCTION
+def main():
     # Générer le paquet de 52 cartes
     paquet_initial = generer_paquet()
     paquet_melange = paquet_initial[:]  # Sans [:], temp agirait comme un pointeur plutôt que de recopier
@@ -52,3 +52,10 @@ if __name__ == "__main__":
     # Afficher le paquet après tri
     afficher_paquet(paquet_melange)
 
+
+
+
+# MAIN GUARD
+if __name__ == "__main__":
+    main()
+    
