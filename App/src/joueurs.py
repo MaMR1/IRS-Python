14c7de7f81c673186c710b0adf2d1c1ea846dbe3
MaMR1 <90766008+MaMR1@users.noreply.github.com>
2024-06-14 14:09:34 +0200

    Update joueurs.py

--- App/src/joueurs.py
+++ App/src/joueurs.py
@@ -5,16 +5,18 @@
 ...
 
 class Joueur:
-    def __init__(self, name, money = int(100), card_1 = None, card_2 = None):
+    def __init__(self, name, money = int(1000), card_1 = None, card_2 = None):
         self.name = name
         self.money = money
         self.card_1 = card_1
         self.card_2 = card_2
-
+        self.mise = 0
+        self.in_game = True  # 'True' si le joueur n'est pas couché ni à sec
+                        
     def __str__(self):
-        return (f"Nom: {self.name}, Solde: {self.money}, Carte 1: {self.card_1}, Carte 2: {self.card_2}")
-    
-#Fonction appelée dans le main pour générer les joueurs en fonction du nombre entré.
+        return (f"Nom: {self.name}, Solde: {self.money}, Mise du tour: {self.mise}, Carte 1: {self.card_1}, Carte 2: {self.card_2}")
+        
+# Fonction appelée dans le main pour générer les joueurs en fonction du nombre entré.
 def generer_joueurs(nombre):
     joueurs = []    # Type : List
     for i in range(nombre):
@@ -23,15 +25,47 @@ def generer_joueurs(nombre):
         joueurs.append(Joueur(name))  
     return joueurs
 
+
 def afficher_joueurs(joueurs):
     for i in joueurs:
         print(i)
+
+
+# Différentes actions que le joueur pour mener
+def se_coucher(joueur):
+    joueur.in_game = False
+    print(f"{joueur.name} se couche...")
+
+
+def suivre(joueur, mise_max):
+    mise = mise_max - joueur.mise
+    joueur.mise += mise
+    joueur.money -= mise
+    print(f"{joueur.name} suit avec : {mise}. Solde restant : {joueur.money}")
+    return mise
+
+
+def miser(joueur, mise_min):
+    
+    while True: # Lorsque la mise sera acceptée, on break
+        # La mise doit obligatoirement être supérieur ou égale à la plus grosse mise
+        mise_min = mise_min - joueur.mise +1
+        mise = int(input(f"{joueur.name}, entrez votre mise (minimum {mise_min}) : "))
+        if mise >= mise_min and mise <= joueur.money:
+            joueur.money -= mise
+            joueur.mise += mise
+            print(f"{joueur.name} relance avec : {mise}. Solde restant : {joueur.money}")
+            return mise
+        else:
+            print(f"Mise invalide. Vous devez miser au moins {mise_min} et au plus {joueur.money}.")
+
 
 
 # MAIN FUNCTIONx
 def main():
     bla = generer_joueurs(5)
     afficher_joueurs(bla)
+    
 
 
 # MAIN GUARD
