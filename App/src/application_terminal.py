15f20718a7249648b96c12fc4d96858ea2bbeadb
MaMR1 <90766008+MaMR1@users.noreply.github.com>
2024-06-14 22:48:17 +0200

    Update application_terminal.py
    Ce fichier dépend de "poker.py" et est appelé par __main__.py

--- App/src/application_terminal.py
+++ App/src/application_terminal.py
@@ -1,38 +1,42 @@
 
-
-#Personnal Modules
+# Personnal Modules
 from application import Application
+import poker
 
 print("Balise : fichier terminal")
-
-
 class Terminal(Application):
     print("Balise : classe terminal")
-    #Attibuts de classe
+    # Attibuts de classe
     project_name = "Projet en Python"
     
     #
     def __init__(self):
+        choix = None
         self.terminal_state = "Chargement en cours...."
-        self.app_launch()
-        #Attributs d'instance
+        self.app_menu()
+              
+        # Attributs d'instance
         #
-        #
-
+        
     #
     def app_launch(self):
-
+        print("Balise : app_launch")
         print(f"Bienvenue sur le {Terminal.project_name}")
-
+        poker.main()
     #
     def app_menu(self):
-
-        print("Menu Principal : Que voulez vous faire ?\n 1) Start Game\n\t 2) Options\n\t 3) Quitter")
-
+        print("Balise : app_menu")
+        match int(input("Menu Principal : Que voulez vous faire ?\n 1) Start Game\n\t 2) Options\n\t\t 3) Quitter\n")):
+            case 1:
+                self.app_launch()
+            case 2:
+                self.app_options()
+            case 3:
+                self.app_close()
     #
     def app_options(self):
-        ...
-    
-    #
+        print("Balise : app_options")
+        #
     def app_close(self):
-        ...
+        exit()
+        
